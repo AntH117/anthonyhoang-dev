4686cddbacf029139051237356d26927944d988b
Anthony <anthony11715@hotmail.com>
2025-11-03 12:59:01 +1100

    Finished Projects grid template

--- anthonyhoang-dev/src/sections/Navbar.js
+++ anthonyhoang-dev/src/sections/Navbar.js
@@ -1,23 +1,25 @@
 import '../styles/Navbar.css';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function Navbar() {
     const navigate = useNavigate();
+    let location = useLocation()
 
     function NavbarSelect({name, callback}) {
-
-        return <div className='navbar-selects' onClick={() => callback()}>
+        const locationName = location.pathname.split('/').at(1)
+        
+        return <div className='navbar-selects' onClick={() => callback()} style={locationName === name.toLowerCase() ? {fontWeight: 'bold'} : {}} >
             {name}
         </div>
     }
 
     return <div className='navbar-body'>
-        <div className='navbar-home'>
+        <div className='navbar-home' onClick={() => navigate('/')}>
             Anthony Hoang
         </div>
         <div className='navbar-selections'>
-            <NavbarSelect name={'Projects'} callback={() => navigate('/projects')}/>
+            <NavbarSelect name={'Projects'} callback={() => navigate('/projects')} />
             <NavbarSelect name={'Skills'}/>
             <NavbarSelect name={'Contact'}/>
         </div>
--- anthonyhoang-dev/src/sections/ProjectsExpanded.js
+++ anthonyhoang-dev/src/sections/ProjectsExpanded.js
@@ -1,9 +1,96 @@
 import '../styles/ProjectsExpanded.css';
+import Navbar from './Navbar';
 import React from 'react';
 
 export default function ProjectsExpanded() {
 
-    return <div>
-        
+    const Projects = [
+        {
+            name: 'CodeIn',
+            description: 'A developer-styled LinkedIn platform for coders featuring authentication, user profiles, and post sharing',
+            tags: ['React', 'Node.js', 'Firebase'],
+            image: ''
+        },
+        {
+            name: 'Pathly',
+            description: 'A trip planner that helps users map out travel routes and attractions using the Google Maps API with custom markers and dynamic directions.',
+            tags: ['React', 'Google Maps API', 'Tailwind CSS'],
+            image: ''
+        },
+        {
+            name: 'Book Finder',
+            description: 'A responsive book discovery app using the Google Books API, allowing users to search, view details, and explore related titles with optimized caching.',
+            tags: ['React', 'API', 'JavaScript'],
+            image: ''
+        },
+        {
+            name: 'Wordle',
+            description: 'A Wordle clone built with React featuring dynamic letter validation, daily word challenges, and clean state management using React Hooks.',
+            tags: ['React', 'Hooks', 'CSS'],
+            image: ''
+        },
+        {
+            name: 'Flight Finder',
+            description: 'A flight tracking app displaying live Australian flight data, delay indicators, and detailed route info using real-time API integration.',
+            tags: ['React', 'API', 'Data Visualization'],
+            image: ''
+        },
+        {
+            name: 'Slider',
+            description: 'A visually engaging website showcasing a smooth sliding animation feature built with React transitions and CSS effects.',
+            tags: ['React', 'CSS', 'Animations'],
+            image: ''
+        }
+    ]
+    
+
+    function EProject({name, description, tags}) {
+
+        function Tag({name}) {
+
+            return <div className='tag-body'>
+                <div className='tag-text'>
+                 {name}
+                </div>
+            </div>
+        }
+
+        return <div className='eProject-body'>
+            <div className='eProject-image'>
+
+            </div>
+            <div className='eProject-info'>
+                <div className='eProject-title'>
+                    {name}
+                </div>
+                <div className='eProject-description'>
+                    {description}
+                </div>
+                <div className='eProject-tags'>
+                    {tags.map((tag) => {
+                        return <Tag name={tag} />
+                    })}
+                </div>
+            </div>
+        </div>
+    }
+
+    return <div className='projects-expanded-container'>
+        <Navbar />
+        <div className='projects-expanded-body'>
+            <div className='projects-expanded-title'>
+                Projects
+            </div>
+            <div className='projects-expanded-main'>
+                Explore a selection of my works
+            </div>
+            <div className='projects-expanded-layout'>
+                {
+                    Projects.map((p) => {
+                        return <EProject name={p.name} description={p.description} tags={p.tags}/>
+                    })
+                }
+            </div>
+        </div>
     </div>
 }

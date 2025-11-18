80a4b80f5eb62df7b95234fddbe3ef14545cfd36
Anthony <anthony11715@hotmail.com>
2025-11-18 12:51:29 +1100

    Completed a draft for the timeline

---
+++ src/data/temp_roles.js
@@ -0,0 +1,25 @@
+
+const tempRoles = [
+    {
+        date: '2020 - 2023',
+        title: 'Elevated Tuition Tutor',
+        description: 'Tutored students in mathematics across various year levels, creating personalised lesson plans and helping students strengthen core concepts, improve confidence, and achieve consistent academic growth.'
+    },
+    {
+        date: '2022',
+        title: 'Business Banking Associate',
+        description: 'Assisted small–medium business clients at Commonwealth Bank, resolving enquiries, supporting loan and account processes, and meeting KPIs by delivering efficient, accurate and high-quality customer service.'
+    },
+    {
+        date: '2023 - Now',
+        title: 'Mathematics Tutor',
+        description: 'Provide 1-on-1 and group tutoring focusing on problem-solving, exam preparation, and long-term skill development. Adapt teaching methods to suit each student and maintain strong communication with families to track progress.'
+    },
+    {
+        date: '2022 - Now',
+        title: 'Self-Study',
+        description: 'Studying software development independently, with a focus on JavaScript, React, frontend development, API integration, portfolio projects, and building practical skills through hands-on coding and real-world applications.'
+    }
+];
+
+export default tempRoles;
--- src/sections/Timeline.js
+++ src/sections/Timeline.js
@@ -1,8 +1,39 @@
 import '../styles/Timeline.css';
 import React from 'react';
 import Navbar from './Navbar';
+import tempRoles from '../data/temp_roles';
 
 export default function Timeline() {
+
+    function TimelineSegment({date, title, desctiption}) {
+        const [expanded, setExpanded] = React.useState(false)
+        const descRef = React.useRef(null)
+
+        React.useEffect(() => {
+            if (expanded && descRef.current) {
+                const height = descRef.current.scrollHeight;
+                descRef.current.style.height = `${height}px`;
+            } else if (descRef.current) {
+                descRef.current.style.height = "0px";
+            }
+        }, [expanded]);
+
+        return <div className='segment-body'>
+            <div className='segment-left'>
+                <div className='segment-date'>
+                    {date}
+                </div>
+            </div>
+            <div className='segment-right'>
+                <div className='segment-title' onClick={() => setExpanded(!expanded)}>
+                    {title}
+                </div>
+                <div className='segment-description' ref={descRef} >
+                    {desctiption}
+                </div>
+            </div>
+        </div>
+    }
 
     return <div className='timeline-outer-body'>
         <div className='timeline-upper-body'>
@@ -13,8 +44,13 @@ export default function Timeline() {
                 From tutoring to full-stack web development, here's a look at my professional growth.
             </div>
         </div>
-        <div className='timeline-graph'>
-
+        <div className='timeline-graph-parent'>
+            <div className='timeline-line'>
+            
+            </div>
+            {tempRoles.map((role) => {
+                return <TimelineSegment date={role.date} title={role.title} desctiption={role.description}/>
+            })}
         </div>
     </div>
 }

bc7253d830ea5e8a0c1518a44ac0e9ded05681b7
Anthony <anthony11715@hotmail.com>
2025-11-20 12:30:50 +1100

    Added subtle hover animations for pins and contacts

--- src/sections/Home_Contact.js
+++ src/sections/Home_Contact.js
@@ -5,6 +5,7 @@ import Icons from '../icons/Icons';
 export default function Home_Contact() {
 
     const linkendin_link = 'https://www.linkedin.com/in/anthony-hoang-2b8578199/'
+    const github_link = 'https://github.com/AntH117'
 
     function ContactField({icon, name, callback}) {
 
@@ -33,6 +34,7 @@ export default function Home_Contact() {
             <div className='contacts-body'>
                 <ContactField icon={<Icons.Email />} name={'anthony.hoang201@gmail.com'} callback={() => null}/>
                 <ContactField icon={<Icons.Linkendin />} name={'Anthony Hoang'} callback={() => window.open(linkendin_link, "_blank")}/>
+                <ContactField icon={<Icons.GitHub />} name={'AntH117'} callback={() => window.open(github_link, "_blank")}/>
             </div>
         </div>
     )
